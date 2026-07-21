419b9650149693a755104970edc1b97e6ac51b8f
BuckyUnlimited <bunnat029@gmail.com>
2026-07-21 11:47:40 +0700

    setup page Auth route

--- frontend-app/src/router/index.js
+++ frontend-app/src/router/index.js
@@ -1,8 +1,37 @@
+import Signup from '@/components/auth/Signup.vue'
+import Signin from '@/components/auth/Signin.vue'
+import Signout from '@/components/auth/Signout.vue'
+import Dashboard from '@/components/pages/Dashboard.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'auth.signin',
+      component: Signin
+    },
+    {
+      path: '/signup',
+      name: 'auth.signup',
+      component: Signup
+    },
+    {
+      path: '/signout',
+      name: 'auth.signout',
+      component: Signout
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: Dashboard
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      redirect: '/dashboard',
+    }
+  ],
 })
 
 export default router
