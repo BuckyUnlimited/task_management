8966f98ba7cdfd8d60b4742b7db2155ce6a097b4
BuckyUnlimited <bunnat029@gmail.com>
2026-07-22 13:33:41 +0700

    setup page Auth on style adminltev4

--- frontend-app/src/components/auth/Signin.vue
+++ frontend-app/src/components/auth/Signin.vue
@@ -1,5 +1,70 @@
 <template>
-  <h1>Signin</h1>
+  <div class="login-page bg-body-secondary app-loaded">
+    <main class="login-box" id="main" tabindex="-1">
+      <h1 class="login-logo">
+        <a href="../index2.html"><b>Admin</b>LTE</a>
+      </h1>
+      <!-- /.login-logo -->
+      <div class="card">
+        <div class="card-body login-card-body">
+          <p class="login-box-msg">Sign in to start your session</p>
+
+          <form action="../index3.html" method="post">
+            <label class="visually-hidden" for="loginEmail">Email</label>
+            <div class="input-group mb-3">
+              <input id="loginEmail" type="email" class="form-control" placeholder="Email">
+              <div class="input-group-text">
+                <span class="bi bi-envelope"></span>
+              </div>
+            </div>
+            <label class="visually-hidden" for="loginPassword">Password</label>
+            <div class="input-group mb-3">
+              <input id="loginPassword" type="password" class="form-control" placeholder="Password">
+              <div class="input-group-text">
+                <span class="bi bi-lock-fill"></span>
+              </div>
+            </div>
+            <!--begin::Row-->
+            <div class="row">
+              <div class="col-8">
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                  <label class="form-check-label" for="flexCheckDefault"> Remember Me </label>
+                </div>
+              </div>
+              <!-- /.col -->
+              <div class="col-4">
+                <div class="d-grid gap-2">
+                  <button type="submit" class="btn btn-primary">Sign In</button>
+                </div>
+              </div>
+              <!-- /.col -->
+            </div>
+            <!--end::Row-->
+          </form>
+
+          <div class="social-auth-links text-center mb-3 d-grid gap-2">
+            <p>- OR -</p>
+            <a href="#" class="btn btn-primary">
+              <i class="bi bi-facebook me-2"></i> Sign in using Facebook
+            </a>
+            <a href="#" class="btn btn-danger">
+              <i class="bi bi-google me-2"></i> Sign in using Google
+            </a>
+          </div>
+          <!-- /.social-auth-links -->
+
+          <p class="mb-1">
+            <a href="forgot-password.html">I forgot my password</a>
+          </p>
+          <p class="mb-0">
+            <router-link :to="{ name: 'auth.signup' }" class="text-center">Register a new membership</router-link>
+          </p>
+        </div>
+        <!-- /.login-card-body -->
+      </div>
+    </main>
+  </div>
 </template>
 
 <script setup></script>
